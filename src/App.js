c86ea03acdd5ebbfe17eed851a351a8f9162e37e
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-27 19:28:50 +0530

    corrected the import statements...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import Footer from "./Components/Pages/Footer";
-import Navbar from "./Components/Pages/Navbar";
-import Shop from "./Components/ProductsPage/ProductPage";
+import Navbar from "./Components/Pages/Navbar/Navbar";
 import AllRoutes from "./Components/Roouters/AllRoutes";
 
 function App() {
